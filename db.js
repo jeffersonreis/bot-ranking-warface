d442ba23edd41a81faadcd2408e76eb9b5f1a871
jeffersonreis <33864468+jeffersonreis@users.noreply.github.com>
2021-11-03 23:44:15 -0300

    abcdef

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const config = require("./config.json");
-const sequelize = new Sequelize(config.BD_URL, {dialect: 'postgres'});
+// const config = require("./config.json");
+const sequelize = new Sequelize("postgres://bhvkswaz:hUW_w78z-HuJ3IYkLxnyA98AVOGxSBIj@motty.db.elephantsql.com/bhvkswaz", {dialect: 'postgres'});
 
 module.exports = sequelize;
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 console.log('Iniciando o bot')
 
 const { Client, Intents } = require('discord.js');
-const config = require("./config.json");
+// const config = require("./config.json");
 const robo = require("./robo.js");
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-client.login(config.BOT_TOKEN);
+client.login("OTA1MjgyNzM5NDY3NTg3NjI2.YYH0QA.3adDsZzBIVKVZoV1uyFzvpeHVGg");
 const prefix = "!";
 
 function lancaBrabra(){
